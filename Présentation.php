e0e54afcc7632e6c80239b1cb01c5e3a519a9dd2
jejlegroudon <jeremiebeauvais91@gmail.com>
2025-03-31 23:21:51 +0200

    Update Présentation.php

--- Présentation.php
+++ Présentation.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
-
-<html lang="fr">
+<?php
+require('fonctions.php');
+session_start();
+bandeau($_SESSION["prenom"]);
+?>
+<html>
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de présentation</title>
    <link rel="stylesheet" href="CSS/Présentation.css">
+   <link rel="stylesheet" href="CSS/Bandeau.css">
 </head>
 <body>
     <div class="div">
