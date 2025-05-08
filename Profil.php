d2324b4c7d5276855f2def5b8b69f12cdb57bae9
johan-1904 <jbultelle@gmail.com>
2025-05-08 23:00:03 +0200

    Update Profil.php

--- Profil.php
+++ Profil.php
@@ -8,7 +8,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="CSS/profil.css">
+	<link rel="stylesheet" type="text/css" href="CSS/Profil.css">
 	<title>Mon compte</title>
 </head>
 <body>
