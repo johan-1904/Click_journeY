d82709d6a03f671a0231d6ca59b021bbfea55fb5
ismael aqouar <skyronebro35@gmail.com>
2025-05-16 16:40:12 +0200

    Add files via upload

--- java/prix.js
+++ java/prix.js
@@ -1,21 +1,35 @@
 function mettreAJourPrix() {
+
+
     const tarifBase = parseFloat(document.getElementById("tarifBase").value) || 0;
     const nb = parseInt(document.getElementById("nb_personnes").value) || 1;
-    const checkboxes = document.querySelectorAll('input[name="options[]"]:checked');
-     let totalOptions = 0;
-   
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    let totalOptions = 0;
 
     checkboxes.forEach(cb => {
         totalOptions += parseFloat(cb.value);
     });
 
     const totalFinal = (totalOptions + tarifBase) * nb;
+    
+    
     document.getElementById("prixOptions").textContent = totalFinal.toFixed(2) + " €";
     document.getElementById("prix_total_input").value = totalFinal.toFixed(2);
- 
+
+    
+    const requete = new XMLHttpRequest();
+    requete.open("POST", "/sauver-prix.php", true); 
+    requete.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    requete.onreadystatechange = function () {
+        if (requete.readyState === 4 && requete.status === 200) {
+            console.log("Réponse du serveur :", totalFinal.toFixed(2));
+        }
+    };
+
+    const params = "prix_total=" + encodeURIComponent(totalFinal.toFixed(2));
+    requete.send(params);
 }
-   window.addEventListener("DOMContentLoaded", mettreAJourPrix);
-   
 
-
+window.addEventListener("DOMContentLoaded", mettreAJourPrix)
 
