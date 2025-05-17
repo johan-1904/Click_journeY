3f26becba254226c2a6eb840842e8865fb4dcdce
johan-1904 <jbultelle@gmail.com>
2025-05-17 22:36:03 +0300

    Add files via upload

--- java/Modif_informations.js
+++ java/Modif_informations.js
@@ -1,17 +1,24 @@
-function Bouton(){
 
-	const formulaire = document.getElementById('modif_info');
-	const boutonModifier = document.getElementById('modif_button');
-	const boutonAnnuler = document.getElementById('annuler_button');
-	Array.from(formulaire.elements).forEach(element => {
-		element.disabled = true;
-	});
-	boutonModifier.addEventListener('click', () => {
-	   	Array.from(formulaire.elements).forEach(element => {
-	     		element.disabled = false;
-	     	});
-	});
-	boutonAnnuler.addEventListener('click', () => {
-		location.reload();
-	});
-}
+document.addEventListener('DOMContentLoaded', function() {
+	const form = document.getElementById('modif-info');
+	const prenom = form.querySelector('input[name="prenom"]').placeholder;
+	const nom = form.querySelector('input[name="nom"]').placeholder;
+	const email = form.querySelector('input[name="email"]').placeholder;
+    const numero = form.querySelector('input[name="numero"]').placeholder;
+	const adresse = form.querySelector('input[name="adresse"]').placeholder;
+
+	const xhr = new XMLHttpRequest();
+    xhr.open("POST", "Modif_informations.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                setTimeout(() => {                          
+            	}, 500);
+			}
+        } 
+    };
+	const params = "email=" + encodeURIComponent(email) + "&prenom=" + encodeURIComponent(prenom) + "&nom=" + encodeURIComponent(nom) + "&adresse=" + encodeURIComponent(adresse) + "&numero=" + encodeURIComponent(numero);
+    xhr.send(params);
+});
